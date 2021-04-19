c326bf190886026569cfa949013298f1f147382e
Damian Płatek <damianpllatek@gmail.com>
2021-04-19 21:33:16 +0200

    Added validations in blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 6, maximum: 50 }
+  validates :content, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :views, presence: true
 end
