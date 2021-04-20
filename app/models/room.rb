eacc86b2c9566dbc62fc7462201477a14582764f
Damian Płatek <damianpllatek@gmail.com>
2021-04-20 21:33:54 +0200

    Created basic validations for room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :size, presence: true
+  validates :base_price, presence: true
 end
