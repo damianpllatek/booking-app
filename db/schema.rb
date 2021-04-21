fb3eb6bd615562dbcf8981322fb8d5afc092b4af
Damian Płatek <damianpllatek@gmail.com>
2021-04-21 17:27:37 +0200

    Created category modell

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_182749) do
+ActiveRecord::Schema.define(version: 2021_04_21_152658) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_04_20_182749) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "seo_title"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "pages", force: :cascade do |t|
