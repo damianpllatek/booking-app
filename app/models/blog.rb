3d2664f59ea657dca653ad54adb87c7e67e668e6
Damian Płatek <damianpllatek@gmail.com>
2021-05-13 12:24:18 +0200

    [WEB-021] Add uploading image to blogs form

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,8 @@ class Blog < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
+  mount_uploader :photo, PhotoUploader
+
   validates :title, presence: true, length: { minimum: 6, maximum: 50 }
   validates :seo_title, presence: true, length: {minimum: 6, maximum: 50 }
   validates :content, presence: true, length: { minimum: 10, maximum: 300 }
