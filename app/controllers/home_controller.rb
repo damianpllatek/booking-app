f0e7b2dd7c692e43fcc754948c8c70cb95101d0d
Damian Płatek <damianpllatek@gmail.com>
2021-05-29 14:07:31 +0200

    [WEB-031] Add blog section to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@
 class HomeController < ApplicationController
   def index
     @rooms = Room.all.order('created_at DESC').limit(3)
+    @blogs = Blog.all.order('created_at DESC').limit(3)
   end
 end
