51ce93be0a84ab7c2d94c29e0efd2c3ae6edecc7
Damian Płatek <damianpllatek@gmail.com>
2021-06-22 17:24:21 +0200

    [WEB-066] Update methods in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@
 class HomeController < ApplicationController
   def index
     @rooms = Room.all.order('created_at DESC').limit(3)
-    @blogs = Blog.all.order('created_at DESC').limit(3)
-    @pages = Page.all.order('created_at DESC').limit(3)
+    @blogs = Blog.all.published.order('created_at DESC').limit(3)
+    @pages = Page.all.published.order('created_at DESC').limit(3)
   end
 end
