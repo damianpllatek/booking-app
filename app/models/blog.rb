81ce39ad4041158caba2dc6a9b02cc6da47a4d08
Damian Płatek <damianpllatek@gmail.com>
2021-06-25 12:46:18 +0200

    [WEB-074] Add category blogs associations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,7 +8,6 @@ class Blog < ApplicationRecord
   validates :seo_title, presence: true, length: {minimum: 6, maximum: 250 }
   validates :content, presence: true, length: { minimum: 10, maximum: 5000 }
 
-  scope :first_scope, -> { where(category: Category.first).where(title: 'test123') }
   scope :published, -> { where(state: 'published') }
   scope :unpublished, -> { where(state: 'unpublished') }
 end
